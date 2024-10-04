2036c12ea6498683ad777c4c5a2fb32c1b8e12d9
sheslop <sheslop@griddynamics.com>
2024-10-04 10:42:16 -0500

    completed task 3

--- JavaCore3/src/main/java/Task1/Task3.java
+++ JavaCore3/src/main/java/Task1/Task3.java
@@ -1,8 +1,11 @@
 package Task1;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+
+import static org.testng.AssertJUnit.assertEquals;
 
 public class Task3 {
     public static void main(String[] args) {
@@ -24,13 +27,40 @@ public class Task3 {
                 .forEach(System.out::println);
 
         //Task 3 question 6 (b
-        System.out.println("Sort all names and display them in lower case on the screen");
+        System.out.println("\n\nSort all names and display them in lower case on the screen");
         memberNames.stream()
                 .map(String::toLowerCase)
                 .sorted(Comparator.naturalOrder())
                 .forEach(System.out::println);
 
         //Task 3 question 7
-        //memberNames.stream().toArray().
+        System.out.println("\n\nFrom task 6, take an array and check with anyMatch or allMatch to find names that contain S");
+        boolean sPresent = memberNames.stream()
+                .anyMatch(x->x.contains("S"));
+        assertEquals(true,sPresent);
+        System.out.println("The list of member names contains the letter S. "+ sPresent);
+
+        System.out.println("\n\nFrom Task 6, take an array and check with noneMatch that the list contains names with the letter H");
+        boolean hIsPresent = memberNames.stream()
+                .noneMatch(x-> x.contains("H"));
+        assertEquals(true,hIsPresent);
+        System.out.println("None of member names in the list contains the letter H. "+ hIsPresent);
+
+        //Task 3 question 8
+        System.out.println("Total number of members are "+ memberNames.stream().count());
+
+        //Task 3 question 9
+        long numOfNamesWithA = memberNames.stream()
+                .filter(x->x.startsWith("A"))
+                .count();
+        System.out.println("There are "+ numOfNamesWithA+" names that begin with the letter A");
+
+        //Task 3 question 10
+        String nameBeginsWithL = memberNames.stream()
+                .filter(x-> x.startsWith("L"))
+                .findFirst()
+                .orElse("No names found beginning with L");
+        System.out.println("The first name that begins with L is: "+ nameBeginsWithL);
+
     }
 }
